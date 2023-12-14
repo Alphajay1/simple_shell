e7b150e54cbfb06f062f5523ddc6611356c9bcd7
Alphajay1 <ayakojamesty@gmail.com>
2023-12-13 18:02:07 -0800

    shell_header.h

--- shell_header.h
+++ shell_header.h
@@ -8,7 +8,12 @@
 #include <sys/types.h>
 #include <string.h>
 
+extern char **environ;
+
 void display_prompt(void);
 void exec_cmd(char *command);
 pid_t check_fork(void);
+void search_path(char *args[]);
+int env_cmd(char **args);
+void env_print(void);
 #endif
